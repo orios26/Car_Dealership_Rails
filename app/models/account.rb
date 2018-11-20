3b2ce7bfe160dd9bb772e28bc6e707edaa1aa791
Otilio Rios <orios@uh.edu>
2018-11-19 20:46:14 -0800

    changed authentication to use devise gem

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,8 +1,6 @@
-class User < ApplicationRecord
+class Account < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :username, presence: true, uniqueness: true
-  has_secure_password
 end
