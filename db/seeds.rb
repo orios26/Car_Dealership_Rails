5cc4f264c91785e8bb9f5a7cebba22d0eb2d5c9b
akshay <kattuaks@gmail.com>
2018-11-23 17:57:25 -0600

    Vehicle image properly displaying

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,20 +19,23 @@
     # User.destroy_all
     #
     # #color seeds
-    # Color.create!([{name: 'Black'}, {name: 'Blue'}, {name: 'Brown'}, {name: 'Gold'}, {name: 'Gray'}, {name: 'Green'},
-    #               {name: 'Orange'}, {name: 'Purple'}, {name: 'Red'}, {name: 'Silver'}, {name: 'Tan'}, {name: 'White'},
-    #               {name: 'Yellow'}])
+    Color.create!([{name: 'Black'}, {name: 'Blue'}, {name: 'Brown'}, {name: 'Gold'}, {name: 'Gray'}, {name: 'Green'},
+                {name: 'Orange'}, {name: 'Purple'}, {name: 'Red'}, {name: 'Silver'}, {name: 'Tan'}, {name: 'White'},
+                 {name: 'Yellow'}])
     #
+
     # #vehicle type seeds
-    # Type.create!([{name: 'Truck'}, {name: 'SUV/Crossover'}, {name: 'Sedan'}, {name: 'Coupe'}, {name: 'Hatchback'},
-    #              {name: 'Van/Minivan'}, {name: 'Convertible'}, {name: 'Wagon'}])
+    Type.create!([{name: 'Truck'}, {name: 'SUV/Crossover'}, {name: 'Sedan'}, {name: 'Coupe'}, {name: 'Hatchback'},
+                {name: 'Van/Minivan'}, {name: 'Convertible'}, {name: 'Wagon'}])
+
+    #Model seeds
+    Model.create!([{name: 'Audi A3'}, {name: 'Audi A4'}, {name: 'Audi A5'}, {name: 'Audi A6'}])
     #
     # #Manager type seeds
     # ManagerType.create!([{name: 'Sales Manager'}, {name: 'Finance Manager'}, {name: 'Inventory Manager'},
     #                      {name: 'Dealership Owner'}])
     #
-    # #Model seeds
-    # Model.create!([{name: 'Audi A3'}, {name: 'Audi A4'}, {name: 'Audi A5'}, {name: 'Audi A6'}])
+
     #
     # #Customer seeds
     # Customer.create!(last_name: 'Grasser', first_name: 'Therin', address: '599 Superior Lane', email: 'tgrasser0@google.com.br', phone: '700-164-4589')
